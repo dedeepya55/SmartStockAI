0476c446d1fcdfd75834cb874216931e1d7d8add
dedeepya55 <22A91A05C8@aec.edu.in>
2025-12-07 18:29:34 +0530

    Initial project upload

---
+++ src/pages/Landing/MainLoginPageHeroSection.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./LandingCSS/MainLoginPageHeroSection.css";
+import heroImage from "../../assets/LandingPageImage1.jpeg"; // replace with your image
+
+const MainLoginPageHeroSection = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-left">
+        <h1 className="hero-title">
+          AI-Powered Insights For Smarter Stock Decisions
+        </h1>
+
+        <p className="hero-subtitle">
+          SmartStock AI helps you analyze markets, predict trends, and make
+          data-driven trading decisions with ease.
+        </p>
+
+        <button className="hero-btn">Get Started</button>
+
+      </div>
+
+      <div className="hero-right">
+        <div className="hero-main-img">
+          <img src={heroImage} alt="Dashboard" />
+        </div>
+
+        <div className="floating-card card-1">
+         Trend Insights
+        </div>
+
+        <div className="floating-card card-2">
+         AI Alerts
+        </div>
+
+        <div className="floating-card card-3">
+           Stock Analysis
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainLoginPageHeroSection;
